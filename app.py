e562d38a4872eaf404ec9d2ce29b2084f1c51e6a
ali777938 <alifande77@gmail.com>
2026-07-15 13:09:56 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -267,4 +267,6 @@ def update_appointment(appointment_id):
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
